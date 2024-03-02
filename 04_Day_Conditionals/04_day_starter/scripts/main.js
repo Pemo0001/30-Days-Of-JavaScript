0c261b00a6f470f912ffd7f84d6a1557e5ac5156
Pemo0001 <pemo0001@stud.kea.dk>
2024-03-02 14:37:37 +0100

    Day 4 DONE

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,121 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+/*
+
+let yourAge = prompt("Enter your age:");
+
+if (yourAge >= 18) {
+  console.log("You are old enough to drive.");
+} else {
+  console.log(`You are left with ${18 - yourAge} years to drive`);
+}
+*/
+
+/*
+
+let yourAge = prompt("Enter your age:");
+let myAge = 28;
+
+if (yourAge > myAge) {
+  console.log(`You are ${yourAge - myAge} years older than me`);
+} else {
+  console.log(`I am ${myAge - yourAge} years older than you`);
+}
+
+*/
+
+/*
+
+let a = 5;
+let b = 4;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a == b) {
+  console.log("a is equal to b");
+} else {
+  console.log("a is less than b");
+}
+*/
+/* 
+let a = 5;
+let b = 6;
+
+a > b ? console.log("a is greater than b") : console.log("a is less than b");
+*/
+
+/*
+
+let myNumber = 5;
+
+if (myNumber % 2 === 0) {
+  console.log(`${myNumber} is an equal number`);
+} else {
+  console.log(`${myNumber} is an odd number`);
+}
+
+*/
+
+/*
+
+let score = 1000;
+
+if (score >= 80) {
+  console.log("You get an A!");
+} else if (score >= 70) {
+  console.log("You get a B!");
+} else if (score >= 60) {
+  console.log("You get a C!");
+} else if (score >= 50) {
+  console.log("You get a D!");
+} else {
+  console.log("You get a F!");
+}
+
+*/
+
+/*
+
+let month = prompt("Which month is it?");
+
+if (month == "September" || month == "November" || month == "December") {
+  console.log("The season is autumn");
+} else if (month == "December" || month == "January" || month == "February") {
+  console.log("The season is winter");
+} else if (month == "March" || month == "April" || month == "May") {
+  console.log("The season is spring!");
+} else if (month == "June" || month == "July" || month == "August") {
+  console.log("The season is summer");
+} else {
+  console.log("That's not a month. Please write the name of a month using a capital first letter");
+}
+
+*/
+
+/*
+
+let day = prompt("Which day is it?").toLowerCase();
+
+if (day == "saturday" || day == "sunday") {
+  console.log(`${day.charAt(0).toUpperCase() + day.slice(1)} is a weekend`);
+} else if (day == "monday" || day == "tuesday" || day == "wednesday" || day == "thursday" || day == "friday") {
+  console.log(`${day.charAt(0).toUpperCase() + day.slice(1)} is a working day`);
+}
+
+*/
+
+/*
+
+let month = prompt("Which month is it?").toLowerCase();
+
+if (month == "january" || month == "march" || month == "may" || month == "july" || month == "august" || month == "october" || month == "december") {
+  console.log(`${month.charAt(0).toUpperCase() + month.slice(1)} has 31 days`);
+} else if (month == "april" || month == "june" || month == "september" || month == "november") {
+  console.log(`${month.charAt(0).toUpperCase() + month.slice(1)} has 30 days`);
+} else if (month == "february") {
+  console.log(`${month.charAt(0).toUpperCase() + month.slice(1)} has 28 days. 29 if it's a leap year.`);
+} else {
+  console.log("That's not a month");
+}
+
+*/
